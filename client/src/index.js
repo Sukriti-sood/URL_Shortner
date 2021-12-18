839646e84d4bc8fdc1bed015841d25016b767ed7
Sukriti-sood <soodsukriti1111@gmail.com>
2021-12-18 17:30:28 +0530

    final

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from "react-dom";
 import App from './App';
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+  } from 'react-router-dom'
 
 
 ReactDOM.render(
 
-    <App />,
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<App />}/>
+    <Route path="*" element={<NotFound/>}/>
+    </Routes>
+    </BrowserRouter>,
    document.getElementById('root')
 );
 
