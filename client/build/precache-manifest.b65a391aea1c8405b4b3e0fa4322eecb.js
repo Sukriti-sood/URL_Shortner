a7aacedc8ca918d3e670646aee8fba114940cd50
Sukriti-sood <soodsukriti1111@gmail.com>
2023-01-08 20:49:07 +0530

    update

---
+++ client/build/precache-manifest.b65a391aea1c8405b4b3e0fa4322eecb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bed748164d4fefe7c7d1c9e43b477c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cd6f3759b4869c1639a",
+    "url": "/static/css/2.d9c51e94.chunk.css"
+  },
+  {
+    "revision": "4cd6f3759b4869c1639a",
+    "url": "/static/js/2.0e2f30da.chunk.js"
+  },
+  {
+    "revision": "fac16c6204ae6b510ba8bcf69209001e",
+    "url": "/static/js/2.0e2f30da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba3e1503fcc72f18bda4",
+    "url": "/static/js/main.79aacf9d.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
